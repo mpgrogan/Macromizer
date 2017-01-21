fdc854faed05c3f3e0effb7de5a1f5031b46074c
Michael Grogan <Michael Grogan>
2017-01-21 12:27:59 -0500

    adding code to repo

---
+++ app/src/main/java/com/example/mpgro/macromizer/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mpgro.macromizer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
